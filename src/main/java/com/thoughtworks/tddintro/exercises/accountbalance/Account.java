314af5c88e7ff5011efbf5048c5e43bdfa254245
Brennan Pilcher <brennan.pilcher@gmail.com>
2018-09-17 01:49:27 -0400

    Wrote the first Account test and the code to make it pass.

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private int balance;
+
+    public Account (int startingBalance) {
+        balance = startingBalance;
+    }
+
+    public void deposit(int value) {
+        balance += value;
+    }
+
+    public int balance() {
+        return balance;
+    }
 }
